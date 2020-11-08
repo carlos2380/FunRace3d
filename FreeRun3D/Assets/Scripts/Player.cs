5a46073f35452b74b094214f4c47c3a01a614159
2380carlos <carlos.2380@hotmail.com>
2020-11-08 18:02:53 +0100

    Turn player

--- FreeRun3D/Assets/Scripts/Player.cs
+++ FreeRun3D/Assets/Scripts/Player.cs
@@ -6,10 +6,12 @@ public class Player : MonoBehaviour
 {
     public float speed;
 
+    private bool isRotation;
+    private float speedRotation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isRotation = false;
     }
 
     // Update is called once per frame
@@ -17,8 +19,26 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Space key is pressed.");
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if (isRotation == true)
+        {
+            transform.Rotate(0, speedRotation * Time.deltaTime, 0);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "RotationPlayer")
+        {
+            speedRotation = other.GetComponent<CtrlRotationPlayer>().rotationSpeedPlayer;
+            isRotation = true;
+        }else if (other.gameObject.tag == "StopRotation")
+        {
+            speedRotation = 0f;
+            isRotation = false;
         }
     }
 }
+
